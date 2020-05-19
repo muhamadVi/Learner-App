7baf0f093ebc7c662a6c0470cc447dd140c34b36
muhamadVi <jayasriwi@gmail.com>
2020-05-19 08:21:25 +0700

    Initial Commit

---
+++ Learner-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Learner-App
+//
+//  Created by Muhamad Vicky on 19/05/20.
+//  Copyright © 2020 Muhamad Vicky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
