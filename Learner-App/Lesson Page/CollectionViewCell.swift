4528b59ff847c4d0e683eeadd6cf0d1e8706d464
muhamadVi <jayasriwi@gmail.com>
2020-05-21 16:27:09 +0700

    Update storboard for lesson learn

---
+++ Learner-App/Lesson Page/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  Learner-App
+//
+//  Created by Muhamad Vicky on 21/05/20.
+//  Copyright © 2020 Muhamad Vicky. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Learner-App/Lesson Page/LessonPage.swift
@@ -0,0 +1,9 @@
+//
+//  LessonPage.swift
+//  Learner-App
+//
+//  Created by Muhamad Vicky on 21/05/20.
+//  Copyright © 2020 Muhamad Vicky. All rights reserved.
+//
+
+import Foundation
