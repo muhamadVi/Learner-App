f9795896e351e778920ef4ed5bc39c5b08b7986e
muhamadVi <jayasriwi@gmail.com>
2020-05-22 15:51:57 +0700

    Add Function to display progreesBar in Lesson Page

--- Learner-App/Lesson Page/LessonPage.swift
+++ Learner-App/Lesson Page/LessonPage.swift
@@ -2,8 +2,29 @@
 //  LessonPage.swift
 //  Learner-App
 //
-//  Created by Muhamad Vicky on 21/05/20.
+//  Created by Muhamad Vicky on 22/05/20.
 //  Copyright © 2020 Muhamad Vicky. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class LessonPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
