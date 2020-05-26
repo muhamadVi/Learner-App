0c05ed4ebcb5ab0a56df7664d765093aed7e0b9e
nmqalbi <62110460+nmqalbi@users.noreply.github.com>
2020-05-26 22:16:21 +0700

    masukin onboarding page

---
+++ Learner-App/onboarding1VC.swift
@@ -0,0 +1,35 @@
+//
+//  onboarding1VC.swift
+//  Learner-App
+//
+//  Created by Nur Minnuri Qalbi on 26/05/20.
+//  Copyright © 2020 Muhamad Vicky. All rights reserved.
+//
+
+import UIKit
+
+class onboarding1VC: UIViewController {
+
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var onboardingAssetsSatu: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func nextButtonPressed(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Learner-App/welcomeVC.swift
@@ -0,0 +1,33 @@
+//
+//  welcomeVC.swift
+//  Learner-App
+//
+//  Created by Nur Minnuri Qalbi on 26/05/20.
+//  Copyright © 2020 Muhamad Vicky. All rights reserved.
+//
+
+import UIKit
+
+class welcomeVC: UIViewController {
+    @IBOutlet weak var getStartedButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func getStartedButtonPressed(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
