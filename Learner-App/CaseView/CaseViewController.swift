f0ce00675defb1fc48589f143f5bf2c2bab7c8e0
rahman <rahmanarif.9764@gmail.com>
2020-05-26 12:47:39 +0700

    Fix storyboard for case and add view controller

---
+++ Learner-App/CaseView/CaseViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CaseViewController.swift
+//  Learner-App
+//
+//  Created by Arif Rahman Sidik on 26/05/20.
+//  Copyright © 2020 Muhamad Vicky. All rights reserved.
+//
+
+import UIKit
+
+class CaseViewController: UIViewController {
+    @IBOutlet weak var ImageViewTopCase: UIImageView!
+    @IBOutlet weak var LabelTitleCase: UILabel!
+    @IBOutlet weak var ImageIconCase: UIImageView!
+    @IBOutlet weak var TableViewCaseContent: UITableView!
+    @IBOutlet weak var TabCase: UITabBarItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
